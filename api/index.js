0d97acfe01d02cda07a24309235896cfdfc0a8ae
Tabawa Renki <112997721+TabawaX@users.noreply.github.com>
2024-06-11 10:45:27 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const app = express()
+const fs = require('fs')
+const path = require('path')
 
-app.get('/', async (req, res) => {
+module.exports = async (req, res) => {
   const indexFile = path.resolve(__dirname, '../public/index.html')
   const cssFile = path.resolve(__dirname, '../public/style.css')
 
   try {
+    
     const [indexRAW, cssRAW] = await Promise.all([
       fs.promises.readFile(indexFile, 'utf8'),
       fs.promises.readFile(cssFile, 'utf8')
@@ -13,16 +14,14 @@ app.get('/', async (req, res) => {
 
     console.log('Successfully read index.html and style.css')
 
+   
     let dataDiEdit = indexRAW.replace(/<head>/, `<head><style>${cssRAW}</style>`)
 
+    // ---- Metode Injeksi Mengirim Request Dengan File Yang Telah Di Injeksi -----------
     res.setHeader('Content-Type', 'text/html')
     res.send(dataDiEdit)
   } catch (err) {
     console.error('Error processing files:', err)
     res.status(500).send('Error processing files')
   }
-})
-
-module.exports = async (req, res) => {
-  return app(req, res)
 }
